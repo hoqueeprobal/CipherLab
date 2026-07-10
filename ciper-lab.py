98b168061fb9cdaa501bc5c9732e04ae4160b606
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2026-07-10 11:07:17 +0600

    Multiplicative Encryption and Update Main File

--- ciper-lab.py
+++ ciper-lab.py
@@ -1,23 +1,33 @@
-from ciphers.additive import encrypt
+from ciphers.additive import encrypt as additive_encrypt
+from ciphers.multiplicative import encrypt as multiplicative_encrypt
 
 
 def main():
 
-    print("      Ciper Lab")
-
+    print("      Cipher Lab\n")
     print("1. Additive Cipher")
+    print("2. Multiplicative Cipher")
     print("0. Exit")
 
     choice = int(input("\nEnter Your Choice: "))
 
     if choice == 1:
+
         plaintext = input("Enter Plaintext: ")
         key = int(input("Enter Key: "))
-        encrypt(plaintext, key)
+        additive_encrypt(plaintext, key)
+
+    elif choice == 2:
+
+        plaintext = input("Enter Plaintext: ")
+        key = int(input("Enter Key: "))
+        multiplicative_encrypt(plaintext, key)
 
     elif choice == 0:
         print("Thank You!")
+
     else:
         print("Invalid Choice!")
+
 
 main()
